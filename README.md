3f2b16ba12fc873996adbb97494ca8cfabc083a0
Raymond <devraymondsh@gmail.com>
2023-04-24 12:46:56 +0330

    Add Ubuntu Lunar(23.04)

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 #### A Rust docker image built on top of Ubuntu Linux.
+
 ## Supported tags:
-- 1.69, 22.10, 22.10-1.69, 22.10-latest, kinetic, kinetic-1.69, kinetic-latest, latest-1.69, latest
+
+- 23.04, 23.04-1.69, lunar-1.69, lunar-latest, lunar, 23.04-latest, latest-1.69, 1.69, latest
+- 22.10, 22.10-1.69, kinetic-1.69, kinetic-latest, kinetic
 - 22.04, 22.04-1.69, jammy-1.69, jammy-latest, jammy
 - 20.04, 20.04-1.69, focal-1.69, focal-latest, focal
 
 ## How to use this image
+
 ### Start a Rust instance running your app
+
 The most straightforward way to use this image is to use a Rust container as both the build and runtime environment. In your Dockerfile, writing something along the lines of the following will compile and run your project:
+
 ```Dockerfile
-FROM devraymondsh/ubuntu-docker-rust:latest
+FROM devraymondsh/ubuntu-rust:latest
 
 WORKDIR /usr/src/myapp
 COPY . .
@@ -17,14 +23,16 @@ RUN cargo install --path .
 
 CMD ["myapp"]
 ```
+
 Then, build and run the Docker image:
 
 `$ docker build -t my-rust-app .`<br />
 `$ docker run -it --rm --name my-running-app my-rust-app`
 
 This creates an image that has all of the rust tooling for the image. If you just want the compiled application which has a smaller size:
+
 ```Dockerfile
-FROM devraymondsh/ubuntu-docker-rust:latest as builder
+FROM devraymondsh/ubuntu-rust:latest as builder
 WORKDIR /usr/src/myapp
 COPY . .
 RUN cargo install --path .
@@ -38,9 +46,11 @@ CMD ["myapp"]
 Note: Some shared libraries may need to be installed as shown in the installation of the extra-runtime-dependencies line above.
 
 ## Compile your app inside the Docker container
+
 There may be occasions where it is not appropriate to run your app inside a container. To compile, but not run your app inside the Docker instance, you can write something like:<br />
-`$ docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/myapp -w /usr/src/myapp devraymondsh/ubuntu-docker-rust:latest cargo build --release`<br />
+`$ docker run --rm --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/myapp -w /usr/src/myapp devraymondsh/ubuntu-rust:latest cargo build --release`<br />
 This will add your current directory, as a volume, to the container, set the working directory to the volume, and run the command cargo build --release. This tells Cargo, Rust's build system, to compile the crate in myapp and output the executable to target/release/myapp.
 
 ## License
-The image is licensed under the MIT license. Visit [LICENSE](https://github.com/devraymondsh/ubuntu-docker-rust/blob/main/LICENSE) for more information.
+
+The image is licensed under the MIT license. Visit [LICENSE](https://github.com/devraymondsh/ubuntu-rust/blob/main/LICENSE) for more information.
--- check-for-update/check-for-update.sh
+++ check-for-update/check-for-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ubuntu-docker-rust
+cd ubuntu-rust
 
 apt-get install --yes libssl-dev pkg-config git
 cargo install rust-latest
@@ -28,7 +28,7 @@ git config --global user.email "github-actions@github.com"
 git config --global --add safe.directory $(realpath .)
 
 function workflow_creator {
-    local repo="devraymondsh/ubuntu-docker-rust"
+    local repo="devraymondsh/ubuntu-rust"
     local rust_version="$1"
     local ubuntu_codename="$2"
     local ubuntu_version="$3"
@@ -36,24 +36,24 @@ function workflow_creator {
 
     local tags=""
 
-    # e.g: devraymondsh/ubuntu-docker-rust:22.10
-    # e.g: devraymondsh/ubuntu-docker-rust:22.10-1.66
+    # e.g: devraymondsh/ubuntu-rust:22.10
+    # e.g: devraymondsh/ubuntu-rust:22.10-1.66
     tags+="$repo:$ubuntu_version,$repo:$ubuntu_version-$rust_version,"
 
-    # e.g: devraymondsh/ubuntu-docker-rust:22.10-latest
-    # e.g: devraymondsh/ubuntu-docker-rust:kinetic
+    # e.g: devraymondsh/ubuntu-rust:22.10-latest
+    # e.g: devraymondsh/ubuntu-rust:kinetic
     tags+="$repo:$ubuntu_version-latest,$repo:$ubuntu_codename,"
 
-    # e.g: devraymondsh/ubuntu-docker-rust:kinetic-1.66
-    # e.g: devraymondsh/ubuntu-docker-rust:kinetic-latest,
+    # e.g: devraymondsh/ubuntu-rust:kinetic-1.66
+    # e.g: devraymondsh/ubuntu-rust:kinetic-latest,
     tags+="$repo:$ubuntu_codename-$rust_version,$repo:$ubuntu_codename-latest"
 
     local workflow=${WORKFLOW_SAMPLE//_UBUNTU_RELEASE_VERSION_/$ubuntu_version}
 
     if $is_the_latest; then
-        # e.g: devraymondsh/ubuntu-docker-rust:1.66,
-        # e.g: devraymondsh/ubuntu-docker-rust:latest, 
-        # e.g: devraymondsh/ubuntu-docker-rust:latest-1.66
+        # e.g: devraymondsh/ubuntu-rust:1.66,
+        # e.g: devraymondsh/ubuntu-rust:latest, 
+        # e.g: devraymondsh/ubuntu-rust:latest-1.66
         tags+=",$repo:$rust_version,$repo:latest,$repo:latest-$rust_version"
 
         workflow+=$DOCKERHUB_DESCRIPTION_UPDATER
@@ -65,7 +65,8 @@ function workflow_creator {
     local file_destination=".github/workflows/docker-$ubuntu_codename.yml"
     echo "$workflow" > "$file_destination"
 }
-workflow_creator "$LATEST_NO_MINOR_RUST_VERSION" "kinetic" "22.10" true
+workflow_creator "$LATEST_NO_MINOR_RUST_VERSION" "lunar" "23.04" true
+workflow_creator "$LATEST_NO_MINOR_RUST_VERSION" "kinetic" "22.10" false
 workflow_creator "$LATEST_NO_MINOR_RUST_VERSION" "jammy" "22.04" false
 workflow_creator "$LATEST_NO_MINOR_RUST_VERSION" "focal" "20.04" false
 
@@ -81,4 +82,4 @@ echo "$LATEST_RUST_VERSION" > "check-for-update/version.txt"
 
 git add .
 git commit -m "Add rust v$LATEST_RUST_VERSION"
-git push "https://devraymondsh:$GITHUB_TOKEN@github.com/devraymondsh/ubuntu-docker-rust.git"
+git push "https://devraymondsh:$GITHUB_TOKEN@github.com/devraymondsh/ubuntu-rust.git"
